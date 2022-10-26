d08f464120f9d682b4c17807365857d112b35cbf
asmeret.gebremariam@kcl.ac.uk <asmeret.gebremariam@kcl.ac.uk>
2022-10-26 22:07:54 +0100

    create model called Thing

--- things/models.py
+++ things/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-#from django.contrib.auth.models import AbstractThing
-# from django.db.models import AbstractThing
 #
-# class Thing(AbstractThing):
-#     thing_name = models.CharField(max_length = 30, blank = False)
-#     thing_description = model.CharField(max_length= 100, blank = False)
-#     #thing_quantity = mo
+class Thing(models.Model):
+    thing_name = models.CharField(max_length = 30, blank = False)
+    thing_description = models.CharField(max_length= 100, blank = False)
+    thing_quantity = models.IntegerField(blank = False)
