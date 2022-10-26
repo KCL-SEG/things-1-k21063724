398789b8222f6af3e6c615fe78f849fde74428d3
asmeret.gebremariam@kcl.ac.uk <asmeret.gebremariam@kcl.ac.uk>
2022-10-26 22:22:01 +0100

    Exercise 3: Refining the Thing model

--- things/models.py
+++ things/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30, blank=False)
-    description = models.CharField(max_length=100, blank=False)
-    quantity = models.IntegerField(blank=False)
+    name = models.CharField(max_length=30, blank=False, unique=True)
+    description = models.CharField(max_length=120, blank=True)
+    quantity = models.IntegerField(unique=False, blank=False)
