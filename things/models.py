88c0f7eecc336a71a7404cc9b48c582f5c269211
asmeret.gebremariam@kcl.ac.uk <asmeret.gebremariam@kcl.ac.uk>
2022-10-26 22:12:16 +0100

    Exercise 2: Add a basic things model

--- things/models.py
+++ things/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#
+
 class Thing(models.Model):
     thing_name = models.CharField(max_length=30, blank=False)
     thing_description = models.CharField(max_length=100, blank=False)
