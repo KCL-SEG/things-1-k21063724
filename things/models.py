58500cd3738bce005c94d5d9fb4753908bcb4815
asmeret.gebremariam@kcl.ac.uk <asmeret.gebremariam@kcl.ac.uk>
2022-10-26 21:13:06 +0100

    Create Things home page

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+#from django.contrib.auth.models import AbstractThing
+# from django.db.models import AbstractThing
+#
+# class Thing(AbstractThing):
+#     thing_name = models.CharField(max_length = 30, blank = False)
+#     thing_description = model.CharField(max_length= 100, blank = False)
+#     #thing_quantity = mo
